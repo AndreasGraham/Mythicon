4705335ba1b638188eb50932b8e5d135f4120ea2
Bryan Hyland <bryan.hyland32@gmail.com>
2018-02-20 17:49:02 -0800

    End Of First Day

---
+++ Assets/Bryan/Scripts/TestUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestUI : MonoBehaviour 
+{
+    [SerializeField] Text instructionText;
+    [SerializeField] GameObject bush;
+
+    bool isPickedUp;
+    bool isTutOver;
+
+	// Use this for initialization
+	void Start () 
+    {
+        isPickedUp = false;
+        isTutOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, bush.transform.position) < 1f)
+        {
+            instructionText.text = "Right Click On The Bush To Pick The Berries";
+            isTutOver = true;
+        }
+
+        if (!isTutOver)
+        {
+            instructionText.text = "Left Click To Walk To The Bush";
+        }
+
+        if (isTutOver)
+            instructionText.enabled = false;
+    }
+}
