275662ffde954eac3d0dfc7701e87e24cc40c329
Carelle <steamtik@gmail.com>
2018-02-20 16:28:51 -0800

    Added in inGame menus and some juice fot the main

---
+++ Assets/Kimberly/[K]Script/[K]Testing/Seek.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour {
+
+    //Rigidbody rb;
+
+    Vector3 desiredVelocity;
+
+    public float speed;
+    public Transform target;
+    // Use this for initialization
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        desiredVelocity = speed * (target.position - transform.position).normalized;
+
+        //rb.AddForce(desiredVelocity - rb.velocity);
+    }
+
+    public Vector3 returnTarget()
+    {
+        return target.position;
+    }
+}
