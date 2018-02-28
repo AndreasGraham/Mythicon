9add9c33886e7ade1038fccb6a0eee710873b9ab
Carelle <steamtik@gmail.com>
2018-02-27 16:06:25 -0800

    Merge branch 'dev' into kimberly

---
+++ Assets/Kimberly/[K]Script/[K]MainMenu/LoadMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMain
+{
+    MainMenu mainMenu = new MainMenu();
+
+    public void getMainMenu()
+    {
+        mainMenu = GameObject.FindGameObjectWithTag("MainMenuCanvas").GetComponent<MainMenu>();
+    }
+
+    public MainMenu loadMainMenu()
+    {
+        return mainMenu;
+    }
+}
---
+++ Assets/Kimberly/[K]Script/[K]MainMenu/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private LoadMain loadMenu = new LoadMain();
+    [SerializeField]
+    private SaveLoadWrapper loading;
+
+    private MainMenu menu;
+    private void Awake()
+    {
+        LoadingScene();
+    }
+    void LoadingScene()
+    {
+        menu = loadMenu.loadMainMenu();
+        loading = Camera.main.GetComponent<SaveLoadWrapper>();
+        if(menu.getIsLoading())
+        {
+            loading.Load("Save1");
+        }
+    }
+}
