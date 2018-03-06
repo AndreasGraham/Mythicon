bc0f1c15e198f52045a4c056c73ca96db0f920e2
Carelle <steamtik@gmail.com>
2018-03-06 09:19:59 -0800

    Repush

--- Assets/Kimberly/[K]Script/[K]MainMenu/Controller.cs
+++ Assets/Kimberly/[K]Script/[K]MainMenu/Controller.cs
@@ -7,94 +7,29 @@ using UnityEngine.SceneManagement;
 public class Controller : MonoBehaviour
 {
     public GameObject inGameMenu;
-    public GameObject TextPanel;
-    public GameObject itemPanel;
-    ItemCount item;
-    bool ingame;
-    public Text itemCount;
-    public Text flowerPick;
-
-   public float timer;
     // Use this for initialization
     void Awake()
     {
-    
         inGameMenu.SetActive(false);
-        ingame = true;
-
-        itemPanel.SetActive(false);
-        TextPanel.SetActive(false);
-        item = gameObject.GetComponent<ItemCount>();
-
-        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            switch (ingame)
+            if (inGameMenu.activeSelf)
             {
-                case true:
-                    inGameMenu.SetActive(true);
-                    Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-                    Time.timeScale = 0;
-                    break;
-                case false:
-                    inGameMenu.SetActive(false);
-                    Time.timeScale = 1;
-                    break;
+                inGameMenu.SetActive(false);
+                Time.timeScale = 1;
             }
-            ingame = !ingame;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            TextPanel.SetActive(true);
-            itemPanel.SetActive(true);
-        }
-
-        if (TextPanel.active == true)
-        {
-
-            flowerPick.text = "Pick Flowers";
-            itemCount.text = "Flowers: " + item.ItemNumber;
-            timer += Time.deltaTime;
-            if (timer >= 5)
+            else
             {
-                TextPanel.SetActive(false);
+                inGameMenu.SetActive(true);
+                Time.timeScale = 0;
             }
         }
-
-        if (item.ItemNumber > 12)
-        {
-            item.ItemNumber = 12;
-        }
-
-        if (item.ItemNumber == 12)
-        {
-            TextPanel.SetActive(true);
-            if (TextPanel.active == true)
-            {
-
-                flowerPick.text = "All flowers have been collected";
-                itemCount.text = "Flowers: Completed";
-                timer += Time.deltaTime;
-            }
-            if (timer >= 5)
-            {
-                TextPanel.SetActive(false);
-                itemPanel.SetActive(false);
-                item.ItemNumber = 0;
-            }
-        }
-        else
-        {
-            timer = 0;
-            TextPanel.SetActive(false);
-        }
+        
     }
 
     public void MainMenu()
@@ -104,7 +39,6 @@ public class Controller : MonoBehaviour
 
     public void ReturnToGame()
     {
-        ingame = false;
         inGameMenu.SetActive(false);
         Time.timeScale = 1;
     }
@@ -112,12 +46,5 @@ public class Controller : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        //if(other.gameObject.tag == )
-        //{
-
-        //}
     }
 }
