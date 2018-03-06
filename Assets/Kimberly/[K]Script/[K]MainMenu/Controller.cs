98be18cdb4fbf08a1df06363be1af2366b6036d1
Carelle <steamtik@gmail.com>
2018-03-06 10:27:13 -0800

    Reworking with the ingame text

--- Assets/Kimberly/[K]Script/[K]MainMenu/Controller.cs
+++ Assets/Kimberly/[K]Script/[K]MainMenu/Controller.cs
@@ -7,11 +7,25 @@ using UnityEngine.SceneManagement;
 public class Controller : MonoBehaviour
 {
     public GameObject inGameMenu;
+    public GameObject pickFlowerPanel;
+    public GameObject flowerNumberPanel;
+    //==================================================
+
+    public Text pickText;
+    public Text counterText;
+    //==================================================
+
+    ItemCounter flowers;
     // Use this for initialization
     void Awake()
     {
         inGameMenu.SetActive(false);
+        pickFlowerPanel.SetActive(false);
+        flowerNumberPanel.SetActive(false);
+
+        flowers = gameObject.GetComponent<ItemCounter>();
     }
+    //==================================================
 
     // Update is called once per frame
     void Update()
@@ -29,19 +43,33 @@ public class Controller : MonoBehaviour
                 Time.timeScale = 0;
             }
         }
-        
+
+    //==================================================
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pickFlowerPanel.SetActive(true);
+            flowerNumberPanel.SetActive(true);
+        }
+
+        if(pickFlowerPanel.activeInHierarchy == true)
+        {
+            pickText.text = "Pick Flowers";
+            counterText.text = "Flowers: " + flowers.item;
+        }
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
     }
+    //==================================================
 
     public void ReturnToGame()
     {
         inGameMenu.SetActive(false);
         Time.timeScale = 1;
     }
+    //==================================================
 
     public void QuitGame()
     {
---
+++ Assets/Kimberly/[K]Script/[K]Testing/ItemCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCounter : MonoBehaviour
+{
+    [SerializeField]
+    public int item;
+
+    public int collectLimit;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            item++;
+        }
+        if(item >= collectLimit)
+        {
+            item = collectLimit;
+        }
+    }
+}
