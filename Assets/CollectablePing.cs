b5cd89ca8c36f04efc1ba3d37dc38e0e8a206b4f
Unknown <s179055@students.aie.edu.au>
2018-03-05 17:54:22 -0800

    First puzzle nearly complete

---
+++ Assets/CollectablePing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectablePing : MonoBehaviour {
+
+    public GameObject collectable;
+    AudioSource secret;
+    bool hasEntered;
+	// Use this for initialization
+	void Start () {
+        secret = GetComponent<AudioSource>();
+        hasEntered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(hasEntered);
+
+        if(hasEntered == false)
+        {
+            if (collectable.activeSelf == true)
+            {
+                secret.Play();
+                hasEntered = true;
+            }
+        }
+
+        else if(hasEntered == true)
+        {
+            hasEntered = false;
+        }
+        
+    }
+}
