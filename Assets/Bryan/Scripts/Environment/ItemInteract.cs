2b8adada79a7b26b284cd546ad3fc92900594f6e
Bryan Hyland <bryan.hyland32@gmail.com>
2018-03-06 11:33:21 -0800

    Working On Village Interaction

---
+++ Assets/Bryan/Scripts/Environment/ItemInteract.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteract : MonoBehaviour 
+{
+    ItemCounter count;
+    PlayerAnimation anim;
+
+    Ray ray;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () 
+    {
+        count = Camera.main.GetComponent<ItemCounter>();
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAnimation>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        CollectItem();
+	}
+
+    void CollectItem()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (hit.collider.gameObject.layer == 12 && Vector3.Distance(transform.position, hit.transform.position))
+            {
+                anim.SetIsPickingUp(true);
+                count.SetItem(2);
+            }
+
+            anim.SetIsPickingUp(false);
+        }
+    }
+}
--- Assets/Bryan/Scripts/Player/Player.cs
+++ Assets/Bryan/Scripts/Player/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
         // Debug Forward
 
