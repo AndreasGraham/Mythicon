f21f4a435afd49bff5eb48028ed5b5ed7bdd768b
Bryan Hyland <bryan.hyland32@gmail.com>
2018-03-07 08:11:24 -0800

    Finished First Puzzle

---
+++ Assets/Bryan/Scripts/Environment/StaffHouse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffHouse : MonoBehaviour 
+{
+    [SerializeField]
+    Collider[] houseColliders;
+
+    CameraManager camManager;
+    Camera mainCamera;
+
+	// Use this for initialization
+	void Start () 
+    {
+        camManager = GameObject.FindGameObjectWithTag("CameraManager").GetComponent<CameraManager>();
+        mainCamera = Camera.main.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (camManager.GetCurrentCamera() == GameObject.Find("Downstairs Entry Staff Camera") && houseColliders[0].enabled)
+        {
+            EnableDisable();
+        }
+
+        if(!mainCamera.enabled)
+        {
+            if (!houseColliders[0].enabled)
+            {
+                EnableDisable();
+            }
+        }
+	}
+
+    void EnableDisable()
+    {
+        foreach (Collider h in houseColliders)
+        {
+            h.enabled = !h.enabled;
+        }
+    }
+}
