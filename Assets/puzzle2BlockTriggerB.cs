4c12414535cb9be3a2e7316913c1a67422e37bf1
Unknown <s179055@students.aie.edu.au>
2018-03-06 17:58:30 -0800

    Finished logic for second puzzle

---
+++ Assets/puzzle2BlockTriggerB.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzle2BlockTriggerB : MonoBehaviour {
+    
+    public static bool colliderTriggerB;
+    // Use this for initialization
+    void Start () {
+        colliderTriggerB = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(colliderTriggerB);
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    colliderTriggerB = true;
+    //    //checks to make sure the correct object is placed, and then checks to make sure the other puzzles colliders are not triggered
+    //    if (other.tag == "puzzle2Green" && puzzle2Manager.red == true && puzzle2BlocktriggerC.colliderTriggerC == false)
+    //    {
+    //        // sets puzzle manager, red bool to true
+    //        puzzle2Manager.green = true;
+    //    }
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        colliderTriggerB = true;
+        //checks to make sure the correct object is placed, and then checks to make sure the other puzzles colliders are not triggered
+        if (other.tag == "puzzle2Green" && puzzle2Manager.red == true && puzzle2BlocktriggerC.colliderTriggerC == false)
+        {
+            // sets puzzle manager, green bool to true
+            puzzle2Manager.green = true;
+           // puzzle2Manager.checkDone();
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        colliderTriggerB = false;
+    }
+
+}
---
+++ Assets/puzzle2BlocktriggerC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzle2BlocktriggerC : MonoBehaviour {
+
+    
+    public static bool colliderTriggerC;
+    // Use this for initialization
+    void Start () {
+        colliderTriggerC = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(colliderTriggerC);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        colliderTriggerC = true;
+        //checks to make sure the correct object is placed, and then checks to make sure the other puzzles colliders are not triggered
+        if (other.tag == "puzzle2Blue" && puzzle2Manager.red == true && puzzle2Manager.green == true)
+        {
+            // sets puzzle manager, blue bool to true
+            puzzle2Manager.blue = true;
+            //puzzle2Manager.checkDone();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        colliderTriggerC = false;
+    }
+}
